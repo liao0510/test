243fcbabbc1da271d9c9c92c53d6d9a87e914be7
Luke Liao <liao051010@gmail.com>
2024-10-06 15:38:52 +0800

    第一次的提交測試

---
+++ script.js
@@ -0,0 +1,12 @@
+function buttonClick() {
+  var button1 = document.getElementById("test")
+  var text1 = "hello"
+  var text2 = "world"
+  button1["value"] = "zzzz"
+  console.log("hihihi")
+  console.info("hihihi")
+  console.warn("hihihi")
+  console.error("hihihi")
+  console.log(`${text1} ${text2}`)
+
+}
